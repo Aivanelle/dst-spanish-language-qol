c25e5414d8cf4adf240a9d46c764107534415222
Aivan <dominguezmiguel715@gmail.com>
2023-06-30 22:23:18 -0700

    Prefabs de herramientas masculinos y femeninos ordenadas.

---
+++ scripts/sortedprefabs/maletools.lua
@@ -0,0 +1,58 @@
+return {
+  SINGULAR =
+  {
+    SUFFIX = STRINGS.WET_SUFFIX.TOOL.MALE.SINGULAR, -- Resbaladizo
+
+    "balloon",
+    "balloonparty",
+    "balloonspeed",
+    "batbat",
+    "blowdart_yellow",
+    "boomerang",
+    "brush",
+    "bullkelp_root",
+    "cane",
+    "cutless",
+    "firestaff",
+    "glasscutter",
+    "gnarwail_horn",
+    "goldenpickaxe",
+    "greenstaff",
+    "hambat",
+    "hammer",
+    "icestaff",
+    "lantern",
+    "lighter",
+    "malbatross_beak",
+    "minifan",
+    "oar",
+    "oar_driftwood",
+    "oar_monkey",
+    "opalstaff",
+    "orangestaff",
+    "pickaxe",
+    "pickaxe_lunarplant",
+    "pocketwatch_weapon",
+    "propsign",
+    "reskin_tool",
+    "ruins_bat",
+    "saddlehorn",
+    "shieldofterror",
+    "slingshot",
+    "staff_lunarplant",
+    "staff_tornado",
+    "telestaff",
+    "tentaclespike",
+    "thurible",
+    "trident",
+    "umbrella",
+    "voidcloth_scythe",
+    "waterballoon",
+    "yellowstaff"
+  },
+
+  PLURAL =
+  {
+    SUFFIX = STRINGS.WET_SUFFIX.TOOL.MALE.PLURAL -- Resbaladizos
+  }
+}
