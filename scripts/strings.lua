87f035681f27bd82e9976e60d92107a62f6dd7c1
Aivan <dominguezmiguel715@gmail.com>
2023-07-03 23:04:45 -0700

    Strings.lua añadido.

---
+++ scripts/strings.lua
@@ -0,0 +1,53 @@
+STRINGS.WET_SUFFIX =
+{
+  FOOD =
+  {
+    MALE = { SINGULAR = "Remojado", PLURAL = "Remojados" },
+    FEMALE = { SINGULAR = "Remojada", PLURAL = "Remojadas" }
+  },
+
+  CLOTHING =
+  {
+    MALE = { SINGULAR = "Empapado", PLURAL = "Empapados" },
+    FEMALE = { SINGULAR = "Empapada", PLURAL = "Empapadas" }
+  },
+
+  TOOL =
+  {
+    MALE = { SINGULAR = "Resbaladizo", PLURAL = "Resbaladizos" },
+    FEMALE = { SINGULAR = "Resbaladiza", PLURAL = "Resbaladizas" }
+  },
+
+  FUEL =
+  {
+    MALE = { SINGULAR = "Mojado", PLURAL = "Mojados" },
+    FEMALE = { SINGULAR = "Mojada", PLURAL = "Mojadas" }
+  },
+
+  GENERIC =
+  {
+    MALE = { SINGULAR = "Húmedo", PLURAL = "Húmedos" },
+    FEMALE = { SINGULAR = "Húmeda", PLURAL = "Húmedas" }
+  }
+}
+
+STRINGS.WAXED_SUFFIX =
+{
+  MALE = { SINGULAR = "Encerado", PLURAL = "Encerados" },
+  FEMALE = { SINGULAR = "Encerada", PLURAL = "Enceradas" }
+}
+
+STRINGS.CREATURE_SUFFIX =
+{
+  HUNGRY =
+  {
+    MALE = { SINGULAR = "Hambriento" },
+    FEMALE = { SINGULAR = "Hambrienta" }
+  },
+
+  STARVING =
+  {
+    MALE = { SINGULAR = "muy Hambriento" },
+    FEMALE = { SINGULAR = "muy Hambrienta" }
+  }
+}
