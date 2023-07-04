a2cbfa42f39143a4c2386d05914afbb7e7cae57b
Aivan <dominguezmiguel715@gmail.com>
2023-07-03 20:23:07 -0700

    Prefabs de vegetales gigantes encerados masculinos y femeninos ordenados.

---
+++ scripts/sortedprefabs/waxedveggies.lua
@@ -0,0 +1,44 @@
+return {
+  MALE =
+  {
+    SINGULAR =
+    {
+      [WAXED_SUFFIX_KEY] = STRINGS.WAXED_SUFFIX.MALE.SINGULAR, -- Emcerado.
+
+      "asparagus_oversized_waxed",
+      "corn_oversized_waxed",
+      "durian_oversized_waxed",
+      "garlic_oversized_waxed",
+      "pepper_oversized_waxed",
+      "tomato_oversized_waxed"
+    },
+
+    PLURAL =
+    {
+      [WAXED_SUFFIX_KEY] = STRINGS.WAXED_SUFFIX.MALE.PLURAL -- Encerados.
+    }
+  },
+
+  FEMALE =
+  {
+    SINGULAR =
+    {
+      [WAXED_SUFFIX_KEY] = STRINGS.WAXED_SUFFIX.FEMALE.SINGULAR, -- Encerada.
+
+      "dragonfruit_oversized_waxed",
+      "eggplant_oversized_waxed",
+      "onion_oversized_waxed",
+      "pomegranate_oversized_waxed",
+      "potato_oversized_waxed",
+      "pumpkin_oversized_waxed",
+      "watermelon_oversized_waxed"
+    },
+
+    PLURAL =
+    {
+      [WAXED_SUFFIX_KEY] = STRINGS.WAXED_SUFFIX.FEMALE.PLURAL, -- Enceradas.
+
+      "carrot_oversized_waxed"
+    }
+  }
+}
