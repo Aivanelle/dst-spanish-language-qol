7afce88670fa50c5cc8a576919177ff3f710bfa7
Aivan <dominguezmiguel715@gmail.com>
2023-07-06 00:45:34 -0700

    _

--- modinfo.lua
+++ modinfo.lua
@@ -2,8 +2,8 @@ name = "Un ajuste al español"
 version = "1.0"
 author = "Aivan"
 description = "Un mod encargado principalmente de mostrar los adjetivos de los objetos en el orden correcto del idioma español.\n" ..
-  "Este mod funciona con cualquier mod de traducción, pero fue hecho para trabajar de una mejor manera con la traducción hecha por joshua_vlad." ..
-  "\n\nVersión " .. version
+  "Este mod funciona con cualquier mod de traducción, pero fue hecho para trabajar de una mejor manera con la traducción hecha por joshua_vlad.\n\n" ..
+  "Versión " .. version
 
 api_version = 10
 
