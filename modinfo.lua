0204ab1fcbc33118772094881631d37631778484
Aivan <dominguezmiguel715@gmail.com>
2023-07-06 00:07:54 -0700

    Descripción actualizada e icono añadido.

--- modinfo.lua
+++ modinfo.lua
@@ -1,18 +1,17 @@
-name = "Spanish tweaks and fixes"
+name = "Un ajuste al español"
 version = "1.0"
 author = "Aivan"
-description = "Un mod encargado de mostrar los adjetivos de los objetos en el orden correcto del idioma español.\nEste mod fue creado para funcionar en conjunto con la traducción al español creada por joshua_vlad.\n\nVersión " .. version
+description = "Un mod encargado principalmente de mostrar los adjetivos de los objetos en el orden correcto del idioma español.\n" ..
+  "Este mod funciona con cualquier mod de traducción, pero fue hecho para trabajar de una mejor manera con la traducción hecha por joshua_vlad." ..
+  "\n\nVersión " .. version
 
 api_version = 10
 
 dst_compatible = true
-
--- Not compatible with Don't Starve
--- dont_starve_compatible = false
--- reign_of_giants_compatible = false
+dont_starve_compatible = false
 client_only_mod = true
 
--- icon_atlas = "modicon.xml"
--- icon = "modicon.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 
 --configuration_options = {}
