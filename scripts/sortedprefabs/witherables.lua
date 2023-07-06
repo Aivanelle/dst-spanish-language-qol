82f6669a5337f90011374b78244576de02d22be7
Aivan <dominguezmiguel715@gmail.com>
2023-07-05 19:29:14 -0700

    Prefabs de objetos marchitables ordenados.

---
+++ scripts/sortedprefabs/witherables.lua
@@ -0,0 +1,37 @@
+return {
+  MALE =
+  {
+    SINGULAR =
+    {
+      [WITHERED_SUFFIX_KEY] = STRINGS.WITHERED_SUFFIX.MALE.SINGULAR,
+
+      "bananabush",
+      "berrybush",
+      "berrybush2",
+      "berrybush_juicy",
+      "plant_normal_ground",
+      "rock_avocado_bush",
+      "sapling",
+      "sapling_moon"
+    }
+  },
+
+  FEMALE =
+  {
+    SINGULAR =
+    {
+      [WITHERED_SUFFIX_KEY] = STRINGS.WITHERED_SUFFIX.FEMALE.SINGULAR,
+
+      "grass",
+      "depleted_grass",
+      "plant_normal"
+    },
+
+    PLURAL =
+    {
+      [WITHERED_SUFFIX_KEY] = STRINGS.WITHERED_SUFFIX.FEMALE.PLURAL,
+
+      "monkeytail"
+    }
+  }
+}
