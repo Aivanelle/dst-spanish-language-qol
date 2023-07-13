7870220347c2fb3c2f76ad6aa9c58a3757ec792c
Aivan <dominguezmiguel715@gmail.com>
2023-07-12 20:36:42 -0700

    Tablas de sufijos reescrita.

--- scripts/strings.lua
+++ scripts/strings.lua
@@ -29,92 +29,95 @@ STRINGS.SPICESMOD =
   }
 }
 
-STRINGS.WET_SUFFIX =
+STRINGS.SUFFIX =
 {
-  FOOD =
+  WET =
   {
-    MALE = { SINGULAR = "Remojado", PLURAL = "Remojados" },
-    FEMALE = { SINGULAR = "Remojada", PLURAL = "Remojadas" }
+    FOOD =
+    {
+      MALE = { SINGULAR = "Remojado", PLURAL = "Remojados" },
+      FEMALE = { SINGULAR = "Remojada", PLURAL = "Remojadas" }
+    },
+
+    CLOTHING =
+    {
+      MALE = { SINGULAR = "Empapado", PLURAL = "Empapados" },
+      FEMALE = { SINGULAR = "Empapada", PLURAL = "Empapadas" }
+    },
+
+    TOOL =
+    {
+      MALE = { SINGULAR = "Resbaladizo", PLURAL = "Resbaladizos" },
+      FEMALE = { SINGULAR = "Resbaladiza", PLURAL = "Resbaladizas" }
+    },
+
+    FUEL =
+    {
+      MALE = { SINGULAR = "Mojado", PLURAL = "Mojados" },
+      FEMALE = { SINGULAR = "Mojada", PLURAL = "Mojadas" }
+    },
+
+    GENERIC =
+    {
+      MALE = { SINGULAR = "Húmedo", PLURAL = "Húmedos" },
+      FEMALE = { SINGULAR = "Húmeda", PLURAL = "Húmedas" }
+    }
   },
 
-  CLOTHING =
+  PERISHABLE =
   {
-    MALE = { SINGULAR = "Empapado", PLURAL = "Empapados" },
-    FEMALE = { SINGULAR = "Empapada", PLURAL = "Empapadas" }
+    STALE =
+    {
+      MALE = { SINGULAR = "Rancio", PLURAL = "Rancios" },
+      FEMALE = { SINGULAR = "Rancia", PLURAL = "Rancias" }
+    },
+
+    SPOILED =
+    {
+      MALE = { SINGULAR = "Caducado", PLURAL = "Caducados" },
+      FEMALE = { SINGULAR = "Caducada", PLURAL = "Caducadas" }
+    }
   },
 
-  TOOL =
+  WAXED =
   {
-    MALE = { SINGULAR = "Resbaladizo", PLURAL = "Resbaladizos" },
-    FEMALE = { SINGULAR = "Resbaladiza", PLURAL = "Resbaladizas" }
+    MALE = { SINGULAR = "Encerado", PLURAL = "Encerados" },
+    FEMALE = { SINGULAR = "Encerada", PLURAL = "Enceradas" }
   },
 
-  FUEL =
+  CREATURE =
   {
-    MALE = { SINGULAR = "Mojado", PLURAL = "Mojados" },
-    FEMALE = { SINGULAR = "Mojada", PLURAL = "Mojadas" }
+    HUNGRY =
+    {
+      MALE = { SINGULAR = "Hambriento" },
+      FEMALE = { SINGULAR = "Hambrienta" }
+    },
+
+    STARVING =
+    {
+      MALE = { SINGULAR = "muy Hambriento" },
+      FEMALE = { SINGULAR = "muy Hambrienta" }
+    }
   },
 
-  GENERIC =
+  PET_TRAIT =
   {
-    MALE = { SINGULAR = "Húmedo", PLURAL = "Húmedos" },
-    FEMALE = { SINGULAR = "Húmeda", PLURAL = "Húmedas" }
+    COMBAT =
+    {
+      MALE = { SINGULAR = "Agresivo" },
+      FEMALE = { SINGULAR = "Agresiva" }
+    },
+  
+    WELLFED =
+    {
+      MALE = { SINGULAR = "Regordete" },
+      FEMALE = { SINGULAR = "Regordeta" }
+    }
+  },
+
+  WITHERED =
+  {
+    MALE = { SINGULAR = "Marchito" },
+    FEMALE = { SINGULAR = "Marchita", PLURAL = "Marchitas" }
   }
-}
-
-STRINGS.WAXED_SUFFIX =
-{
-  MALE = { SINGULAR = "Encerado", PLURAL = "Encerados" },
-  FEMALE = { SINGULAR = "Encerada", PLURAL = "Enceradas" }
-}
-
-STRINGS.CREATURE_SUFFIX =
-{
-  HUNGRY =
-  {
-    MALE = { SINGULAR = "Hambriento" },
-    FEMALE = { SINGULAR = "Hambrienta" }
-  },
-
-  STARVING =
-  {
-    MALE = { SINGULAR = "muy Hambriento" },
-    FEMALE = { SINGULAR = "muy Hambrienta" }
-  }
-}
-
-STRINGS.PERISHABLE_SUFFIX =
-{
-  STALE =
-  {
-    MALE = { SINGULAR = "Rancio", PLURAL = "Rancios" },
-    FEMALE = { SINGULAR = "Rancia", PLURAL = "Rancias" }
-  },
-
-  SPOILED =
-  {
-    MALE = { SINGULAR = "Caducado", PLURAL = "Caducados" },
-    FEMALE = { SINGULAR = "Caducada", PLURAL = "Caducadas" }
-  }
-}
-
-STRINGS.PET_TRAIT_SUFFIX =
-{
-  COMBAT =
-  {
-    MALE = { SINGULAR = "Agresivo" },
-    FEMALE = { SINGULAR = "Agresiva" }
-  },
-
-  WELLFED =
-  {
-    MALE = { SINGULAR = "Regordete" },
-    FEMALE = { SINGULAR = "Regordeta" }
-  }
-}
-
-STRINGS.WITHERED_SUFFIX =
-{
-  MALE = { SINGULAR = "Marchito" },
-  FEMALE = { SINGULAR = "Marchita", PLURAL = "Marchitas" }
 }
