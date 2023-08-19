584e11ed5952ca14e0f1c4a1028fbe63b6823753
Aivan <dominguezmiguel715@gmail.com>
2023-08-18 21:56:14 -0700

    Archivo renombrado.

---
+++ scripts/stringsmod.lua
@@ -0,0 +1,134 @@
+STRINGS.NAMES.BLUEPRINTMOD = "Planos para {item}"
+
+STRINGS.SPICESMOD =
+{
+  SPICE_CHILI =
+  {
+    DRAGONCHILISALAD = "Ensalada Extrapicante de Dragón",
+    HOTCHILI = "Chile Extrapicante",
+
+    GENERIC = "{food} Picantes"
+  },
+
+  SPICE_SUGAR =
+  {
+    DRAGONCHILISALAD = "Ensalada Dulce de Dragón",
+    HOTCHILI = "Chile Dulce",
+
+    GENERIC = "{food} Dulces"
+  },
+
+  SPICE_SALT =
+  {
+    GENERIC = "{food} y Sal"
+  },
+
+  SPICE_GARLIC =
+  {
+    GENERIC = "{food} y Ajo"
+  }
+}
+
+STRINGS.SUFFIX =
+{
+  WET =
+  {
+    FOOD =
+    {
+      MASCULINE = { SINGULAR = "Remojado", PLURAL = "Remojados" },
+      FEMININE = { SINGULAR = "Remojada", PLURAL = "Remojadas" }
+    },
+
+    CLOTHING =
+    {
+      MASCULINE = { SINGULAR = "Empapado", PLURAL = "Empapados" },
+      FEMININE = { SINGULAR = "Empapada", PLURAL = "Empapadas" }
+    },
+
+    TOOL =
+    {
+      MASCULINE = { SINGULAR = "Resbaladizo", PLURAL = "Resbaladizos" },
+      FEMININE = { SINGULAR = "Resbaladiza", PLURAL = "Resbaladizas" }
+    },
+
+    FUEL =
+    {
+      MASCULINE = { SINGULAR = "Mojado", PLURAL = "Mojados" },
+      FEMININE = { SINGULAR = "Mojada", PLURAL = "Mojadas" }
+    },
+
+    GENERIC =
+    {
+      MASCULINE = { SINGULAR = "Húmedo", PLURAL = "Húmedos" },
+      FEMININE = { SINGULAR = "Húmeda", PLURAL = "Húmedas" }
+    }
+  },
+
+  PERISHABLE =
+  {
+    STALE =
+    {
+      MASCULINE = { SINGULAR = "Rancio", PLURAL = "Rancios" },
+      FEMININE = { SINGULAR = "Rancia", PLURAL = "Rancias" }
+    },
+
+    SPOILED =
+    {
+      MASCULINE = { SINGULAR = "Caducado", PLURAL = "Caducados" },
+      FEMININE = { SINGULAR = "Caducada", PLURAL = "Caducadas" }
+    }
+  },
+
+  WAXED =
+  {
+    MASCULINE = { SINGULAR = "Encerado", PLURAL = "Encerados" },
+    FEMININE = { SINGULAR = "Encerada", PLURAL = "Enceradas" }
+  },
+
+  CREATURE =
+  {
+    HUNGRY =
+    {
+      MASCULINE = { SINGULAR = "Hambriento" },
+      FEMININE = { SINGULAR = "Hambrienta" }
+    },
+
+    STARVING =
+    {
+      MASCULINE = { SINGULAR = "muy Hambriento" },
+      FEMININE = { SINGULAR = "muy Hambrienta" }
+    }
+  },
+
+  PET_TRAIT =
+  {
+    COMBAT =
+    {
+      MASCULINE = { SINGULAR = "Agresivo" },
+      FEMININE = { SINGULAR = "Agresiva" }
+    },
+  
+    WELLFED =
+    {
+      MASCULINE = { SINGULAR = "Regordete" },
+      FEMININE = { SINGULAR = "Regordeta" }
+    }
+  },
+
+  WITHERED =
+  {
+    MASCULINE = { SINGULAR = "Marchito" },
+    FEMININE = { SINGULAR = "Marchita", PLURAL = "Marchitas" }
+  },
+
+  SMOLDERING =
+  {
+    NEUTRAL = { SINGULAR = "Llameante", PLURAL = "Llameantes" }
+  },
+
+  BROKEN =
+  {
+    MASCULINE = { SINGULAR = "Roto" },
+    FEMININE = { SINGULAR = "Rota" }
+  }
+}
